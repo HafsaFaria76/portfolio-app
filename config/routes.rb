a3624fa33ce7bb38229fbba7c06a15e647b7b602
usman sajid <usman.sajid@square63.org>
2021-08-23 16:14:37 +0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  match '/home', to: 'static_pages#home', via: 'get', as: :home
+  match '/academic', to: 'static_pages#academic', via: 'get', as: :academic
+  match '/contact', to: 'static_pages#contact', via: 'get', as: :contact
 end
