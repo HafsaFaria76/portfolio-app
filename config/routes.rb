d8a60b44b35f41a30ee26d43b01b209508e35de0
usman sajid <usman.sajid@square63.org>
2021-08-26 13:35:38 +0500

    added name to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  match '/home', to: 'static_pages#home', via: 'get', as: :home
-  match '/academic', to: 'static_pages#academic', via: 'get', as: :academic
-  match '/contact', to: 'static_pages#contact', via: 'get', as: :contact
+  match 'hafsa-faria/home', to: 'static_pages#home', via: 'get', as: :home
+  match 'hafsa-faria/academic', to: 'static_pages#academic', via: 'get', as: :academic
+  match 'hafsa-faria/contact', to: 'static_pages#contact', via: 'get', as: :contact
 end
